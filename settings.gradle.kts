ef58e21a8aa6b1d95f51eb1c046409c383652a8c
arshiyaazim <arshiya.azim.1980@gmail.com>
2025-05-09 23:45:33 +0600

    Fix: Add firebase-perf plugin version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,17 @@
 pluginManagement {
     repositories {
         google()
-        mavenCentral()
         gradlePluginPortal()
-    }
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
+    }
+
+    plugins {
+        id("com.android.application") version "8.2.0"
+        id("com.android.library") version "8.2.0"
+        id("org.jetbrains.kotlin.android") version "1.9.20"
+        id("dagger.hilt.android.plugin") version "2.50"
+        id("com.google.gms.google-services") version "4.4.1"
+        id("com.google.firebase.crashlytics") version "2.9.9"
+        id("com.google.firebase.firebase-perf") version "1.4.2" // ✅ ADD THIS
     }
 }
-
-rootProject.name = "OneLove"
-include(":app")
